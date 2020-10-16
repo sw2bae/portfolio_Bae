7a1d74dcb56e8f12776a7a5b3f880283d98a3bb8
Sang Bae <sw2.bae@gmail.com>
2020-10-15 19:09:55 -0500

    rev09_footer Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Skill from "./components/skill";
 import About from "./components/about";
 import Resume from "./components/resume";
 import Portfolio from "./components/portfolio";
+import Footer from "./components/footer";
 
 import './App.css';
 
@@ -23,36 +24,27 @@ function App() {
 
       <section className="contact">
         <h2 className="contact_title">Get in Touch</h2>
-        <div className="contact_point">
-          <p>Feel free to reach out. I'd love to hear from you!</p>
-          <ul>
-            <li>sw2.bae@gmail.com</li>
-            <li>214.727.4326</li>
-          </ul>
+        <div className="contact_container">
+
+          <div className="contact_point">
+            <p>Feel free to reach out. I'd love to hear from you!</p>
+            <ul>
+              <li>sw2.bae@gmail.com</li>
+              <li>214.727.4326</li>
+            </ul>
+          </div>
+
+          <form className="contact_input">
+            <input className="emailInput"></input>
+            <input className="nameInput"></input>
+            <input className="bodyInput"></input>
+          </form>
         </div>
-        <form className="contact_input">
-          <input className="emailInput"></input>
-          <input className="nameInput"></input>
-          <input className="bodyInput"></input>
-        </form>
         <button className="contact_btn">Send</button>
       </section>
 
-
-      <footer>
-        <div className="footer_logo">
-          <i className="fas fa-battery-full"></i>
-          <span>SANG BAE</span>
-        </div>
-        <div className="footer_right">
-          <span>Copyright 2020. All rights reserved.</span>
-        </div>
-        <div className="footer_icon">
-          <i className="fab fa-github-square"></i>
-          <i className="fab fa-linkedin"></i>
-        </div>
-      </footer>
-    </Router>
+      <Footer />
+    </Router >
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <div className="footer_logo">
+                <Link onClick={() => {
+                    window.scrollTo(0, 0)
+                }}><i class="fab fa-connectdevelop"></i>
+                    <span className="footer_point">BAE</span></Link>
+            </div>
+            <div className="footer_right">
+                <span>Copyright 2020. All rights reserved.</span>
+            </div>
+            <div className="footer_icon">
+                <a href="https://github.com/sw2bae" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-github-square"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/sang-bae-8a38011a0/" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
