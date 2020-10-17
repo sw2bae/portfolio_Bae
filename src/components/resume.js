dd85de249359ff691e6178e9a0500cb7e25b5284
Sang Bae <sw2.bae@gmail.com>
2020-10-16 21:33:30 -0500

    rev18_resume uploaded

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Profile from "../SangBae_Profile.pdf";
 
 function Resume() {
 
@@ -14,7 +15,7 @@ function Resume() {
                 <h2 className="resume_title">Like What You See?</h2>
                 <p>Check out my resume to learn more</p>
             </div>
-            <a className="resume_btn" href="#" target="_blank" rel="noopener noreferrer"><i className="far fa-file" onMouseEnter={black} onMouseLeave={white}></i></a>
+            <a className="resume_btn" href={Profile} target="_blank" rel="noopener noreferrer"><i className="far fa-file" onMouseEnter={black} onMouseLeave={white}></i></a>
         </section>
     );
 }
